726ce1996804d7ac8ef83387984fcff2de24d448
nguyenluatt <162718520+nguyenluatt@users.noreply.github.com>
2025-05-29 22:38:35 +0700

    Add files via upload

---
+++ FirstDegree/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+
+void first_degree_solution(double a, double b)
+{
+    if (a == 0 & b == 0)
+    {
+        Console.WriteLine("vo so nghim");
+    }
+    else if (a == 0 & b != 0)
+    {
+        Console.WriteLine("vo nghim");
+    }
+    else
+    {
+
+        Console.WriteLine("X={0}", -b / a);
+    }
+}
+Console.OutputEncoding = Encoding.UTF8;
+Console.WriteLine("pt bac 1: ax+b=0");
+Console.WriteLine("nhap he so a");
+double a = double.Parse(Console.ReadLine());
+Console.WriteLine("nhap he so b");
+double b = double.Parse(Console.ReadLine());
+Console.WriteLine("{0}+{1}=0",a,b);
+first_degree_solution(a, b);
+Console.ReadLine();
---
+++ FirstProject/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Text;
+
+Console.OutputEncoding = Encoding.UTF8;
+Console.WriteLine("xinchao");
+Console.ForegroundColor= ConsoleColor.Red;
+Console.WriteLine("met");
+Console.ForegroundColor = ConsoleColor.Green;
+Console.BackgroundColor= ConsoleColor.Black;
+Console.WriteLine("met dien");
+Console.ReadLine();
